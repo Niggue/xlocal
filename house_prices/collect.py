ba5b2fe09c74cd5f324125633fd28dec0525af32
niggue <miguedjba96@gmail.com>
2022-11-11 23:35:41 -0500

    ~ : add finca raiz collecting code segment

--- house_prices/collect.py
+++ house_prices/collect.py
@@ -21,16 +21,18 @@ options = webdriver.ChromeOptions()
 options.page_load_strategy = 'normal'
 options.add_argument('--incognito')
 options.add_argument('--disable-gpu')
-options.add_argument('--window-position=960,0')
+options.add_argument('--window-position=0,0')
+options.add_argument('--window-size=0,0')
 options.add_argument('--blink-settings=imagesEnabled=false')
-options.add_argument('--headless')
+#options.add_argument('--headless')
 
 local_proxy_options = webdriver.ChromeOptions()
 local_proxy_options.add_argument('--incognito')
 local_proxy_options.add_argument('--disable-gpu')
-local_proxy_options.add_argument('--window-position=960,0')
+local_proxy_options.add_argument('--window-position=0,0')
+local_proxy_options.add_argument('--window-size=0,0')
 local_proxy_options.add_argument('--blink-settings=imagesEnabled=false')
-local_proxy_options.add_argument('--headless')
+#local_proxy_options.add_argument('--headless')
 
 
 
@@ -48,8 +50,8 @@ baseurl = [
 ]
 
 cities = [
-    'bogota','barrancabermeja','barranquilla','buenaventura',
-    'cali','cartagena','medellin','mompos','riohacha','santa-marta',
+    'mompos','barrancabermeja','barranquilla','buenaventura',
+    'cali','cartagena','medellin','bogota','riohacha','santa-marta',
     'turbo','tumaco'
 ]
 
@@ -74,6 +76,12 @@ page_number_box_class = "page-item"
 page_active_class = "page-item active disabled"
 
 
+### Finca raiz tag_classes
+# 'div' tags that contains 'a' tags
+finra_link_class = "MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-6 MuiGrid-grid-lg-4 MuiGrid-grid-xl-4"
+# 'button' tags to click on
+finra_page_number_class = "MuiButtonBase-root MuiPaginationItem-root MuiPaginationItem-page MuiPaginationItem-outlined MuiPaginationItem-rounded"
+
 
 ### Functions Scope ###
 #=======================================================================================================================================
@@ -85,11 +93,13 @@ def clattr(__class_name): # this function adapt the class attributes strings for
 
 
 
+
 def write_log(__message, __log_file="collect.log"): # this write and print info about the runtime status
 
     with open(__log_file, "a") as log:
         log.write(f"{__message}\n")
         print(__message)
+
 
 
 
@@ -103,23 +113,23 @@ def try_get(__url): # this function try 3 times with different proxies, if none 
         # setting options for each retry
         if (attempts != 3):
             proxy = proxyr.roll_proxy(proxyr._proxies, module="se")
-            options.add_argument(f"--proxy-server={proxy}")
+            #options.add_argument(f"--proxy-server={proxy}")
             __driver = webdriver.Chrome(options=options, service=Service(executable_path=ChromeDriverManager().install()))
-            __driver.implicitly_wait(5)
+            __driver.minimize_window()
+            __driver.implicitly_wait(1.5)
         else:
             __driver = webdriver.Chrome(options=local_proxy_options, service=Service(executable_path=ChromeDriverManager().install()))
-            __driver.implicitly_wait(5)
+            __driver.minimize_window()
+            __driver.implicitly_wait(1.5)
             write_log("Using local Proxy: ")
 
-        # getting the main list tag of the DoM where lies all the tag that we are gonna use along the whole script
+        # getting the main list tag of the DOM where lies all the tag that we are gonna use along the whole script
         # all the 'li' tags constains the 'a' tag that will be stored in a csv formatted file, to scrap later in
         # other separated script.
         try:
             __driver.get(__url)
-            if (__driver.find_element(By.CLASS_NAME, clattr(item_list_class)).is_displayed() &
-                    __driver.find_element(By.CLASS_NAME, clattr(page_number_class)).is_displayed()):
-                write_log("Got it!")
-                break
+            write_log("Got it!")
+            break
         except:
             write_log("Site can't be reached, Retrying")
 
@@ -127,6 +137,7 @@ def try_get(__url): # this function try 3 times with different proxies, if none 
         __driver.close()
 
     return __driver
+
 
 
 
@@ -149,37 +160,39 @@ def collect_metrocuadrado(__data):    # this gather links for scraping from 'met
 
             # gathering data and writing into gather.dat
             while (True):
-
+                
                 # finding list of items
                 li_tags = driver.find_elements(By.CLASS_NAME, clattr(item_list_class))
                 # finding links of items, I mean "href" attributes
-                for item in range(len(li_tags)):
-                    item_to_append = li_tags[item].find_element(By.CLASS_NAME, clattr(item_link_class))
+                for li in li_tags:
+                    item_to_append = li.find_element(By.CLASS_NAME, clattr(item_link_class))
                     a_tags.append(item_to_append.get_attribute("href"))
 
                 # writing log for more info about process
                 current_page += 1
-                info = f" L[{baseurl[0].split('/')[-1]}:page:{current_page}] a:href links got > {len(a_tags)}"
+                info = f" L[{baseurl[0].split('/')[-1]} : page : {current_page} : {city_item}] a:href links got > {len(a_tags)}"
                 write_log(info)
 
                 #  getting all the page item links
-                page_index = driver.find_elements(By.CLASS_NAME, clattr(page_number_box_class))
-                
+                try:
+                    page_index = driver.find_elements(By.CLASS_NAME, clattr(page_number_box_class))
 
-                # finding the 'current page' and seek for the 'next page element' to click on
-                for i in range(len(page_index)):
-                    if (page_index[i].get_attribute("class") == page_active_class):
-                        page_next = page_index[i + 1]
+                    # finding the 'current page' and seek for the 'next page element' to click on
+                    for i in range(len(page_index)):
+                        if (page_index[i].get_attribute("class") == page_active_class):
+                            page_next = page_index[i + 1]
+                            break
+
+                    # exiting the loop in case 'page next' were the last page listed
+                    if (page_next.get_attribute("class") == "item-icon-next page-item disabled"):
                         break
 
-                # exiting the loop in case 'page next' were the last page listed
-                if (page_next.get_attribute("class") == "item-icon-next page-item disabled"):
+                    # clicking on the 'next page element', to redirect scraping the next page
+                    next_page_element = page_next.find_element(By.TAG_NAME, "a")
+                    driver.execute_script("arguments[0].click();", next_page_element)
+                except:
                     break
 
-                # clicking on the 'next page element', to redirect scraping the next page
-                next_page_element = page_next.find_element(By.TAG_NAME, "a")
-                driver.execute_script("arguments[0].click();", next_page_element)
-            
             # closing the browser session
             driver.quit()
 
@@ -195,45 +208,102 @@ def collect_metrocuadrado(__data):    # this gather links for scraping from 'met
     
 
 
+
 def collect_fincaraiz(__data):
     
+
+
     if (__data == None):
         raise AttributeError("You haven't passed '__data' argument")
-    # setting the url structure
-    finra_url = "%s/%ss/%s/%s" % (baseurl[1], facility[0], via, cities[9])
+
+    for facility_item in facility:
+        for city_item in cities:
+            # declaring the variables that we'll use, and the 'data' dictionary that will be returned
+            total_pages = 0
+            a_tags = []
+
+            # setting the url structure
+            finra_url = "%s/%ss/%s/%s?pagina=1" % (baseurl[1], facility_item, via, city_item)
+
+            # getting the total of pages, since the website index pages with the url 
+            # trying the connection
+            driver = try_get(finra_url)      
+
+            # checks wheather one page only
+            try:
+                page_index = driver.find_elements(By.CLASS_NAME, clattr(finra_page_number_class))
+                last_page = int(page_index[-2].text)
+            except:
+                page_index = False
+            
+            # runtime depend of page_index
+            if (page_index != False):
+
+                for pg in range(1, (last_page + 1)):
+
+                    # setting the url structure
+                    finra_url = "%s/%ss/%s/%s?pagina=%d" % (baseurl[1], facility_item, via, city_item, pg)
+                    # passing toward next page
+                    driver.get(finra_url)
+                    
+                    # getting all the 'div' container of 'a' tags to extract href links
+                    div_tags = driver.find_elements(By.CLASS_NAME, clattr(finra_link_class))
+                    
+                    # extracting links
+                    for div in div_tags:
+                        try:
+                            item_to_append = div.find_element(By.TAG_NAME, "a").get_attribute("href")
+                        except:
+                            item_to_append = False   # some 'div' tags has sponsor, so we need skip them
+
+                        if (item_to_append != False):
+                            a_tags.append(item_to_append)
+                    
+                    info = f" L[{baseurl[1].split('/')[-1]} : page : {pg} : {city_item}] a:href links got > {len(a_tags)}"
+                    write_log(info)
+            
+            else:
     
-    # trying the connection
-    driver = try_get(finra_url)
+                write_log("no page index: must be just one page")
+                # setting the url structure
+                finra_url = "%s/%ss/%s/%s?pagina=%d" % (baseurl[1], facility_item, via, city_item, int(1))
+                # passing toward next page
+                driver.get(finra_url)
+                
+                # getting all the 'div' container of 'a' tags to extract href links
+                div_tags = driver.find_elements(By.CLASS_NAME, clattr(finra_link_class))
+                
+                # extracting links
+                for div in div_tags:
+                    try:
+                        item_to_append = div.find_element(By.TAG_NAME, "a").get_attribute("href")
+                    except:
+                        continue    # some 'div' tags has sponsor, so we need skip them
+                    else:
+                        a_tags.append(item_to_append)
+                
+                info = f" L[{baseurl[1].split('/')[-1]}:page:Unica] a:href links got > {len(a_tags)}"
+                write_log(info)
 
-    # declaring the variables that we'll use, and the 'data' dictionary that will be returned
-    current_page = 0
-    a_tags = []
 
-    
-    # gathering data and writing into gather.dat
-    while (False):
+            # closing the browser session
+            driver.quit()
 
+            # Appending info to data variable to save in 'gather.dat'
+            write_log(f"Appending links for {baseurl[1]} ... ")
+            
+            for a in a_tags:
+                __data['href'].append(a)
+                __data['facility'].append(facility_item)
+                __data['city'].append(city_item)
+                __data['website'].append(baseurl[1]) 
+                __data['code'].append(a.split("/")[-1])
 
-
-
-    # closing the browser session
-    driver.quit()
-
-    # Appending info to data variable to save in 'gather.dat'
-    write_log(f"Appending links for {baseurl[0]} ... ")
-    
-    for a in a_tags:
-        __data['href'].append(a)
-        __data['facility'].append(facility_item)
-        __data['city'].append(city_item)
-        __data['website'].append(baseurl[0]) 
-        __data['code'].append(a.split("/")[-1])
 
 
 
 def collect_puntopropiedad(__data):
 
-    
     if (__data == None):
         raise AttributeError("You haven't passed '__data' argument")
     # setting the url structure
@@ -249,7 +319,7 @@ def collect_puntopropiedad(__data):
     
     # gathering data and writing into gather.dat
     while (False):
-
+        pass
 
 
 
@@ -293,13 +363,13 @@ if __name__=="__main__":
     task1.start()
     
     task2 = Thread(target=collect_fincaraiz, args=([data]))
-    task2.run()
+    task2.start()
 
-    task1.join()
-    task2.join()
+    task1.join(timeout=float(21800))
+    task2.join(timeout=float(21800))
     
     
-
+    # Saving to CSV file
     try:
         df = pandas.DataFrame(
             data=data,
